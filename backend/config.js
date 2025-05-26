aef0e30cd9682703f3890e88a90b8135ad7ae958
Samir Rajani <122851375+Samir-Rajani@users.noreply.github.com>
2025-05-26 04:41:46 -0230

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = ####;
 
 export const mongoDBURL =
-    'mongodb+srv://root:root@book-store-mern.aemsaos.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
+    '...'
